773f5b129b3ae2998818fa70a72e2aaa8cf06e15
Achirawit Kaewkhong <freewinendlife@gmail.com>
2026-09-13 02:55:51 +0700

    perf(admin): avoid duplicate auth fetch in auth layout
    Refs #11

--- apps/admin/src/app/(web)/(auth)/layout.tsx
+++ apps/admin/src/app/(web)/(auth)/layout.tsx
@@ -1,4 +1,3 @@
-import { auth } from '@/auth'
 import { AppProvider } from '@/components/providers/app-provider'
 import { createPreloadedState } from '@/lib/store'
 import type { ReactNode } from 'react'
@@ -6,9 +5,8 @@ import type { ReactNode } from 'react'
 export default async function AuthenticationLayout({
   children
 }: Readonly<{ children: ReactNode }>) {
-  const session = await auth()
   const preloadedState = await createPreloadedState(
-    { user: session?.user ?? null },
+    { user: null },
     []
   )
 
